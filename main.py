d6315d016af09bd7987519355ce1e9fafcae2f52
pukhrajgedar31-code <pukhrajgedar31@gmail.com>
2026-04-06 17:18:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -418,5 +418,7 @@ def handle_upload():
     file.save(save_path)
     return jsonify({"status": "success", "file_path": save_path})
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
